fd21c859b6a400a4b78aa4a049439ce7617cdd0f
SemenovBohdan123 <nadgbosemenov@gmail.com>
2023-01-04 11:39:32 +0200

    use long pulling

--- src/LongPulling.jsx
+++ src/LongPulling.jsx
@@ -6,6 +6,22 @@ const LongPulling = () => {
   const [messages, setMessages] = useState([]);
   const [value, setValue] = useState('');
 
+  useEffect(() => {
+    subscribe()
+  }, [])
+
+  const subscribe = async () => {
+    try {
+      const { data } = await axios.get('http://localhost:5000/get-messages')
+      setMessages((prev) => [data, ...prev])
+      await subscribe()
+    } catch (error) {
+      setTimeout(() => {
+        subscribe()
+      }, 500);
+    }
+  }
+
   const sendMessage = async () => {
     await axios.post('http://localhost:5000/new-messages', {
       message: value,
